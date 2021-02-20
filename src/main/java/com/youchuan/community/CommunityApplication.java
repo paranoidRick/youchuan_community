cd51baeee882d88bef4babe9f2d4e8fa9aa66d47
yangyouchuan <1184540833@qq.com>
2021-02-20 21:45:22 +0800

    add bmsBillboardController

--- src/main/java/com/youchuan/community/CommunityApplication.java
+++ src/main/java/com/youchuan/community/CommunityApplication.java
@@ -1,8 +1,10 @@
 package com.youchuan.community;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.youchuan.community.mapper")
 @SpringBootApplication
 public class CommunityApplication {
 
