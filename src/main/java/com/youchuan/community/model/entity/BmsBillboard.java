3eac14d4afd81bd258225432ad513c0170201a92
yangyouchuan <1184540833@qq.com>
2021-02-20 21:21:18 +0800

    add billboard

---
+++ src/main/java/com/youchuan/community/model/entity/BmsBillboard.java
@@ -0,0 +1,48 @@
+package com.youchuan.community.model.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.Date;
+
+// @Builder 支持链式操作 @Accessors 支持链式操作
+@Data
+@Builder
+@Accessors(chain = true)
+@TableName("bms_billboard")
+@NoArgsConstructor
+@AllArgsConstructor
+public class BmsBillboard implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 公告牌
+     */
+    @TableField("content")
+    private String content;
+
+    /**
+     * 公告时间
+     */
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 1：展示中，0：过期
+     */
+    @Builder.Default
+    @TableField("`show`")
+    private boolean show = false;
+}
---
+++ src/main/java/com/youchuan/community/service/impl/IBmsBillboardServiceImpl.java
@@ -0,0 +1,12 @@
+package com.youchuan.community.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.youchuan.community.mapper.BmsBillboardMapper;
+import com.youchuan.community.model.entity.BmsBillboard;
+import com.youchuan.community.service.IBmsBillboardService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IBmsBillboardServiceImpl extends ServiceImpl<BmsBillboardMapper, BmsBillboard> implements IBmsBillboardService {
+
+}
